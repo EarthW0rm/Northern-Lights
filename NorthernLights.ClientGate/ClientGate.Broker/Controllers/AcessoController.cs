b95f5815465fff86b707ebd589ecc5681e1a861e
marco_faustino@hotmail.com <marco_faustino@hotmail.com>
2018-02-21 19:27:21 -0300

    Incluido uma controler MVC que servirá como API para login.

---
+++ NorthernLights.ClientGate/ClientGate.Broker/Controllers/AcessoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClientGate.Broker.Controllers
+{
+    [Route("[controller]")]
+    public class AcessoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
