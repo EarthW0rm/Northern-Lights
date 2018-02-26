f6efc046930bea83fc5db3cd9e65d012900ba897
EarthW0rm <marco_faustino@hotmail.com>
2018-02-25 21:55:15 -0300

    Alteracoes nos initis do mongo para incluir usuario na inicializacao.

--- NorthernLights.ClientGate/ClientGate.Repository/docker/mongo-db/mongo_initial_script.js
+++ NorthernLights.ClientGate/ClientGate.Repository/docker/mongo-db/mongo_initial_script.js
@@ -1,3 +1,7 @@
+db = db.getSiblingDB('admin');
+db.createUser({ user: "northernlight", pwd: "minh0c@", roles: [{ role: "userAdminAnyDatabase", db: "admin" }] });
+db.createUser({ user: "sa", pwd: "sa", roles: [{ role: "userAdminAnyDatabase", db: "admin" }] });
+
 db = db.getSiblingDB('NL_CLIENTGATE');
 print('NL_CLIENTGATE database created');
 
